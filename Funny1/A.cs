a4079b58de5fe271eb34c7e63498133d508d6912
eduoo <44001810+eduoo@users.noreply.github.com>
2018-10-14 11:26:01 +0300

    Add project files.

---
+++ Funny1/A.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funny1
+{
+    public class A
+    {
+        public virtual void Prompt()
+        {
+            Console.WriteLine("Hello from A...");
+        }
+    }
+
+    public class B : A
+    {
+        public override void Prompt()
+        {
+            Console.WriteLine("Hello from B...");
+        }
+    }
+
+    public class C : B
+    {
+        public override void Prompt()
+        {
+            Console.WriteLine("Hello from C...");
+        }
+    }
+}
---
+++ Funny1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funny1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A o1 = new A();
+            o1.Prompt();
+
+            B o2 = new B();
+            o2.Prompt();
+
+            C o3 = new C();
+            o3.Prompt();
+
+            A o4 = new C();
+            o4.Prompt();
+
+            A o5 = new B();
+            o5.Prompt();
+
+            B o6 = new C();
+            o6.Prompt();
+
+            //C o7 = new A();
+
+            Console.ReadLine();
+        }
+    }
+}
